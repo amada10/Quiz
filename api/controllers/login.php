3275987f7411ed2808c0a0900a58ed7bb3399f2e
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-02-01 09:21:37 +0300

    fichier de login...!

--- api/controllers/login.php
+++ api/controllers/login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 class ControllersLogin {
     private array $data;
     public function __construct(string $identifiant, string $keyword) {
@@ -12,6 +13,14 @@ class ControllersLogin {
         $login = new LoginAdmin();
         $resultats = $login -> authentifierAdmin($this -> data);
         unset($login);
+        print_r(json_encode($resultats, JSON_FORCE_OBJECT));
+    }
+
+    public function sessionLogin() {
+        $login = new LoginAdmin();
+        $resultats = $login -> authentifierAdmin($this -> data);
+        unset($login);
+        $_SESSION['infos'] = $resultats;
         print_r(json_encode($resultats, JSON_FORCE_OBJECT));
     }
 }
